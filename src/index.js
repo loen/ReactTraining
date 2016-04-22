db26551b6275f6c236cbd685f52b3b394fe405c9
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-04-23 00:36:55 +0200

    added video_detail

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import SearchBar from './Components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './Components/video_list';
+import VideoDetail from './Components/video_detail';
 const YOU_TUBE_API_KEY = 'AIzaSyBGYV6zZJ5-uiUARDS9byRmm3tUXEL0toM';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
-    YTSearch({ key: YOU_TUBE_API_KEY, term: 'barcelona' }, (videos) => {
-      this.setState({ videos });
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    YTSearch({ key: YOU_TUBE_API_KEY, term: 'barcelona city' }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -19,6 +26,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
