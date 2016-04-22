1b8162bb9b0c0cac9df58363175670f25cb9e61d
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-04-23 00:52:47 +0200

    added video selection

--- src/Components/video_list.js
+++ src/Components/video_list.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const videoItems = videos.map((video) => {
-    return <VideoListItem key={video.etag} video={video} />;
+    return <VideoListItem onVideoSelect={onVideoSelect} key={video.etag} video={video} />;
   });
   return (
     <ul className="col-md-4 list-group">
--- src/index.js
+++ src/index.js
@@ -26,8 +26,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}/>
       </div>
     );
   };
