a85fd3f1edc5d8432c8d6854c7c105168bd4d85f
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-04-23 00:00:23 +0200

    added video_list and video_list_item

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './Components/search_bar';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './Components/video_list';
 const YOU_TUBE_API_KEY = 'AIzaSyBGYV6zZJ5-uiUARDS9byRmm3tUXEL0toM';
 
-const App = function () {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({ key: YOU_TUBE_API_KEY, term: 'barcelona' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  };
 };
 
 ReactDOM.render(
