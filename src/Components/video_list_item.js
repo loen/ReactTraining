92898fe12c33734f9db30c9a4fb2e6b3fa3acf7f
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-05-11 23:01:53 +0200

    Just check Atom integration

--- src/Components/video_list_item.js
+++ src/Components/video_list_item.js
@@ -8,6 +8,7 @@ const VideoListItem = ({ video, onVideoSelect }) => {
       onClick={ () => onVideoSelect(video)}
       className='list-group-item'>
       <div className='video-list media'>
+      
         <div className='media-left'>
           <img className='media-object' src={imgUrl} />
         </div>
